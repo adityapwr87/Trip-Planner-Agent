ae52a07cb5a4595cfbb31ad6e21cac44f90d04bf
adityapwr87 <adityapwr87@gmail.com>
2026-06-30 14:35:10 +0530

    Update frontend API base URL to use env variables

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create a configured axios instance
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: process.env.REACT_APP_API_URL || "http://127.0.0.1:8000",
 });
 
 // Add a request interceptor to attach the token
